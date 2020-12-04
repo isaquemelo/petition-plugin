86d7cc84cf43a9c39cdf1a6bcb888a9e36a978cd
Isaque Melo <isaquegbmelo2@gmail.com>
2020-12-04 12:05:42 -0300

    Add sidebar import to petition

--- petitions.php
+++ petitions.php
@@ -11,6 +11,7 @@ include __DIR__ . '/library/admin-columns.php';
 include __DIR__ . '/library/assets.php';
 include __DIR__ . '/library/ajax.php';
 include __DIR__ . '/library/metaboxes.php';
+include __DIR__ . '/library/register-sidebars.php';
 // include __DIR__ . '/pagebuilder.php';
 
 add_filter('template_include', 'petitions_single_template');
