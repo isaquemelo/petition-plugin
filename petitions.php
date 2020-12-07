2f516808146c417d4ecf4f995b4515e29cd3175e
Isaque Melo <isaquegbmelo2@gmail.com>
2020-12-07 15:03:45 -0300

    Add admin script import

--- petitions.php
+++ petitions.php
@@ -12,6 +12,7 @@ include __DIR__ . '/library/assets.php';
 include __DIR__ . '/library/ajax.php';
 include __DIR__ . '/library/metaboxes.php';
 include __DIR__ . '/library/register-sidebars.php';
+include __DIR__ . '/library/export-signatures.php';
 // include __DIR__ . '/pagebuilder.php';
 
 add_filter('template_include', 'petitions_single_template');
@@ -44,4 +45,68 @@ function get_languages($petition_id) {
 	$results = $wpdb->get_results("SELECT post_id FROM {$wpdb->prefix}postmeta WHERE meta_key = 'petition_parent' and meta_value = '{$petition_id}'", OBJECT);
 
 	return $results;
+}
+
+// Add action hook only if action=download_csv
+if ( isset($_GET['action'] ) && $_GET['action'] == 'download_csv' )  {
+	// Handle CSV Export
+	add_action( 'admin_init', 'csv_export' );
+}
+
+function csv_export() {
+    // Check for current user privileges 
+    // if( !current_user_can( 'manage_options' ) ){ return false; }
+
+    // Check if we are in WP-Admin
+    if( !is_admin() ){ return false; }
+
+    // Nonce Check
+    // $nonce = isset( $_GET['_wpnonce'] ) ? $_GET['_wpnonce'] : '';
+    // if ( ! wp_verify_nonce( $nonce, 'download_csv' ) ) {
+    //     die( 'Security check error' );
+    // }
+    
+    ob_start();
+
+    $domain = $_SERVER['SERVER_NAME'];
+    $filename = 'petition-signatures-' . $domain . '-' . time() . '.csv';
+    
+    $header_row = array(
+        'Email',
+		'Name',
+		'Country',
+		'Keep me Updated?',
+		'Date'
+	);
+	
+    $data_rows = array();
+    global $wpdb;
+    $sql = 'SELECT * FROM ' . $wpdb->users;
+	$users = $wpdb->get_results( $sql, 'ARRAY_A' );
+	
+    foreach ( $users as $user ) {
+        $row = array(
+            $user['user_email'],
+            $user['user_name']
+        );
+        $data_rows[] = $row;
+	}
+	
+    $fh = @fopen( 'php://output', 'w' );
+    fprintf( $fh, chr(0xEF) . chr(0xBB) . chr(0xBF) );
+    header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+    header( 'Content-Description: File Transfer' );
+    header( 'Content-type: text/csv' );
+    header( "Content-Disposition: attachment; filename={$filename}" );
+    header( 'Expires: 0' );
+    header( 'Pragma: public' );
+    fputcsv( $fh, $header_row );
+    foreach ( $data_rows as $data_row ) {
+        fputcsv( $fh, $data_row );
+    }
+    fclose( $fh );
+    
+    ob_end_flush();
+    
+    die();
 }
