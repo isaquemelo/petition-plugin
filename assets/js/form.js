7b2257ec260e89c2eb1ea09b5451c0ae4ffbc9ba
Isaque Melo <isaquegbmelo2@gmail.com>
2020-12-08 16:21:29 -0300

    Remove legacy code

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,37 +1,3 @@
-// (function($) {
-//     $(document).ready(function(){
-//         // setTimeout(function() {
-//         //     if(grecaptcha){
-//         //         grecaptcha.execute()
-//         //     }
-//         // }, 2000);
-        
-//         $('#formPetition').on('submit', function(e){
-//             e.preventDefault();
-            
-//             if($('#formPetition .custom-checkbox input').length > 0 && !$('#formPetition .custom-checkbox input').is(':checked')){
-//                 $('#formPetition .custom-checkbox').addClass('error');
-//                 return false;
-//             }
-//             $.post('/petitions/'+ $('#formPetition').data('petition-id') +'/create', $('#formPetition').serialize() , function(data){
-//                 $('#formPetition input').val('');
-//                 $('#modal-petition .modal-petition-content').show();
-//                 $('#modal-petition .modal-content-stf').hide();
-//                 $('#modal-petition-wrapper, #modal-petition').addClass('show');
-//                 $('#formPetition .custom-checkbox').removeClass('error');
-//                 $('#formPetition .custom-checkbox input').trigger('click');
-//             })
-
-//             return false;
-//         });
-
-//         $('#modal-petition .close-button').click(function(){
-//             $('#modal-petition-wrapper, #modal-petition').removeClass('show');
-//         })
-        
-//     });
-// })(jQuery);
-
 function pushFormData(data) {
     return fetch(petitionPlugin.url + 'wp-json/petition/sign', {
         method: 'post',
