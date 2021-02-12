d49f9fc9d32fa02ccfa4e826899eb6be5615ff19
moisesmsa <moises78zito@hotmail.com>
2021-02-12 16:37:56 -0300

    fixed checkbox value

--- assets/js/form.js
+++ assets/js/form.js
@@ -45,9 +45,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Form submition
     const petitionForm = document.querySelector('#petition-form');
-    showSignature = document.getElementsByName('show_signature')[0].checked;
     const skipedInputs = ['accept-terms', 'show_signature'];
-    const defaultValues = { 'keep_me_updated': false, 'show_signature': showSignature};
+    const defaultValues = { 'keep_me_updated': false};
 
     // Status area
     const counting = document.querySelector('.signatures-count .quantity span:first-child');
@@ -66,8 +65,11 @@ window.addEventListener('DOMContentLoaded', function() {
 
     if(petitionForm) {
         petitionForm.addEventListener('submit', function(e) {
-            const formData = new FormData(petitionForm);
             const filteredData = {};
+            const formData = new FormData(petitionForm);
+            const showSignatureChecked = document.getElementsByName('show_signature')[0].checked;
+            
+            filteredData['show_signature'] = showSignatureChecked;
 
             for (let [key, value] of formData.entries()) {
                 // Skip some fields
@@ -102,8 +104,8 @@ window.addEventListener('DOMContentLoaded', function() {
                             if(progressArea) {
                                 progressArea.style.width = (parseInt(progressCounting.innerHTML) / goal) * 100 + "%";
                             }
-                            console.log("show: "+showSignature);
-                            if(showSignature){
+
+                            if(showSignatureChecked){
                                 const newSignature = document.createElement('div');
                                 newSignature.classList.add('user-signature');
                                 newSignature.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-user fa-w-14 fa-3x"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z" class=""></path></svg>' 
