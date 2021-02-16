2eb4b012fafae0ddcf48b759c476f69ceeddce75
moisesmsa <moises78zito@hotmail.com>
2021-02-16 17:28:23 -0300

    select petition component

--- src/petition-block.js
+++ src/petition-block.js
@@ -2,32 +2,49 @@ import { __ } from '@wordpress/i18n'
 import { SelectControl } from '@wordpress/components';
 import { withState } from '@wordpress/compose';
 
-registerBlockType('petitions/petition-block', {
+const { Component } = wp.element;
+const { withSelect, select } = wp.data;
+
+class FirstBlockEdit extends Component {
+
+    render() {
+        let choices = [];
+        if (this.props.posts) {
+            choices.push({ value: 0, label: __('Select a post', 'petition') });
+            this.props.posts.forEach(post => {
+                choices.push({ value: post.id, label: post.title.rendered });
+            });
+        } else {
+            choices.push({ value: 0, label: __('Loading...', 'petition') })
+        }
+
+        return (<SelectControl
+        label={__('Petition', 'petition')}
+        options={choices}
+        value={this.props.attributes.selectedPostId}
+        onChange={(newval) => setAttributes({ selectedPostId: parseInt(newval) })}
+        />);
+    }
+}
+
+wp.blocks.registerBlockType('petitions/petition-block', {
 	title: 'Petiton block',
 	category: 'common',
 	icon: 'smiley',
 	description: 'Insert a petition block anywhere',
 	keywords: ['petition', 'petitions'],
-	edit: () => { 
-		return <div>
-                    <SelectControl
-                        multiple
-                        label={ __( 'Select some users:' ) }
-                        value={ this.state.users } // e.g: value = [ 'a', 'c' ]
-                        onChange={ ( users ) => { this.setState( { users } ) } }
-                        options={ [
-                            { value: null, label: 'Select a User', disabled: true },
-                            { value: 'a', label: 'User A' },
-                            { value: 'b', label: 'User B' },
-                            { value: 'c', label: 'User c' },
-                        ] }
-                    />
-                </div>
+    attributes: {
+        selectedPostId:{
+            type: 'interger',
+        },
+    },
+	edit: withSelect(select => {
+            return {
+                posts: select('core').getEntityRecords('postType', 'petition')
+            }
+        })(FirstBlockEdit),
+	
+    save: (props) => { 
+		return <div>:)</div> 
 	},
-	save: () => { 
-		return 
-        <div>
-           <p>conte</p>   
-        </div> 
-	}
 });
